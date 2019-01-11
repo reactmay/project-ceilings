fc8c0a5b880e3197785faa83e1f8f2d1f2058770
Ivan <zugurysus@gmail.com>
2019-01-12 04:06:28 +1000

    Создал главный контроллер и шаблон

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function execute(Request $request) {
+        return view('layouts.site');
+    }
+}
